21e2733897914b09ef79d122d938d08383332438
Simon Brookes <2011834+smabuk@users.noreply.github.com>
2019-08-25 12:25:32 +0100

    Add appsettings files and configure db connection

--- src/smabPlayground2019/Server/Startup.cs
+++ src/smabPlayground2019/Server/Startup.cs
@@ -1,19 +1,35 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
+using smabPlayground2019.Server.Data;
 using System.Linq;
 
 namespace smabPlayground2019.Server
 {
 	public class Startup
 	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlite(
+					Configuration.GetConnectionString("DefaultConnection")));
+			services.AddDefaultIdentity<IdentityUser>()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
 			services.AddMvc().AddNewtonsoftJson();
 			services.AddResponseCompression(opts =>
 			{
